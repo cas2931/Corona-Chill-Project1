8edf912333e8f7b5d37a205b5e87afe650b0a36b
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-24 14:35:50 -0400

    several changes to contents of cs-project-branch1

---
+++ corona+chill-CS-DD-Slideshow.js
@@ -0,0 +1,7 @@
+// Array of all movies in playlist
+var D_D_Movies = ['Outbreak','Contagion','28 Days Later','28 Weeks Later','12 Monkeys','I Am Legend',
+                  'Rise of the Planet of the Apes','The Andromeda Strain','Cabin Fever','Flu','Deranged','Blindness'];  
+// Array might require stringification to be used in a AJAX call, made variable just in case
+//var D_D_json = JSON.stringify(D_D_Movies);
+
+for(i = 0; i < D_D_Movies.length; i++) {
