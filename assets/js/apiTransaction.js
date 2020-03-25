68d126b30fbfdf5f2662434fa2843933ff582837
sanjida nawrin <sanjidanawrin123@gmail.com>
2020-03-24 20:38:55 -0400

    added new app page

---
+++ assets/js/apiTransaction.js
@@ -0,0 +1,44 @@
+// Initial Values
+const MOVIE_DB_API = '62f39555ad4630e6aa093145adbd1abc';
+const MOVIE_DB_ENDPOINT = 'https://api.themoviedb.org';
+const MOVIE_DB_IMAGE_ENDPOINT = 'https://image.tmdb.org/t/p/w500';
+const DEFAULT_POST_IMAGE = 'https://via.placeholder.com/150';
+
+function requestMovies(url, onComplete, onError) {
+    fetch(url)
+        .then((res) => res.json())
+        .then(onComplete)
+        .catch(onError);
+}
+
+function generateMovieDBUrl(path) {
+    const url = `${MOVIE_DB_ENDPOINT}/3${path}?api_key=${MOVIE_DB_API}`;
+    return url;
+}
+
+// funtion to generate top rated movies
+function getTopRatedMovies() {
+    const url = generateMovieDBUrl(`/movie/top_rated`);
+    const render = renderMovies.bind({ title: 'Top Rated Movies' })
+    requestMovies(url, render, handleGeneralError);
+}
+
+//funtion to generate top upcoming movies
+function searchUpcomingMovies() {
+    const url = generateMovieDBUrl('/movie/upcoming');
+    const render = renderMovies.bind({ title: 'Upcoming Movies' })
+    requestMovies(url, render, handleGeneralError);
+}
+
+//a different function for search movies
+function searchMovie(value) {
+    const url = generateMovieDBUrl('/search/movie') + '&query=' + value;
+    requestMovies(url, renderSearchMovies, handleGeneralError);
+}
+
+// funtion to find videos based on movie id
+function getVideosByMovieId(movieId, content) {
+    const url = generateMovieDBUrl(`/movie/${movieId}/videos`);
+    const render = createVideoTemplate.bind({ content });
+    requestMovies(url, render, handleGeneralError);
+}
