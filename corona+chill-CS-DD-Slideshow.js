0e6be2fef6206c504fa99e9870b846b4517eec6d
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-25 21:34:19 -0400

    renamed slideshow css file. Added to slideshow js file

--- corona+chill-CS-DD-Slideshow.js
+++ corona+chill-CS-DD-Slideshow.js
@@ -1,62 +1,44 @@
+$(document).ready(function ($) {
 // Array of all movies in playlist
-// var D_D_Movies = ['Outbreak','Contagion','28 Days Later','28 Weeks Later','12 Monkeys','I Am Legend',
-//                   'Rise of the Planet of the Apes','The Andromeda Strain','Cabin Fever','Flu','Deranged','Blindness'];  
-// // Array might require stringification to be used in a AJAX call, made variable just in case
-// //var D_D_json = JSON.stringify(D_D_Movies);
+var D_D_Movies = ['Outbreak','Contagion','28 Days Later','28 Weeks Later','12 Monkeys','I Am Legend',
+                  'Rise of the Planet of the Apes','The Andromeda Strain','Cabin Fever','Flu','Deranged','Blindness'];  
 
-// for(i = 0; i < D_D_Movies.length; i++) { 
+// Array might require stringification to be used in a AJAX call, made variable just in case
+//var D_D_json = JSON.stringify(D_D_Movies); 
 
-//   var imbbAPI = "c45c8f7d90msh2875850ffd544ffp1dc202jsn45c649435346"
-//   var imdbURL = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/" + D_D_Movies[i] +   
-  
-//   $.ajax({
-//     url: imdbURL,
-//     method: "GET"
-//   }).then(function(response)  
+//Loop to run through array
+for (i = 0; i < D_D_Movies.length; i++)
 
-  $(document).ready(function ($) {
+// Ajax call settings. Looks hinky compared to other api setups, but it was the way the maker set up the code snippet.
+// The method & url are the same, and the api key is already integrated in as a plus. 
+var AC_settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + D_D_Movies[0],
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+		"x-rapidapi-key": "c45c8f7d90msh2875850ffd544ffp1dc202jsn45c649435346"
+	}
+}
 
-    $('#checkbox').change(function(){
-      setInterval(function () {
-          moveRight();
-      }, 3000);
-    });
-    
-    var slideCount = $('#slider ul li').length;
-    var slideWidth = $('#slider ul li').width();
-    var slideHeight = $('#slider ul li').height();
-    var sliderUlWidth = slideCount * slideWidth;
-    
-    $('#slider').css({ width: slideWidth, height: slideHeight });
-    
-    $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-    
-      $('#slider ul li:last-child').prependTo('#slider ul');
+$.ajax(AC_settings).done(function (response) {
+  console.log(response);
   
-      function moveLeft() {
-          $('#slider ul').animate({
-              left: + slideWidth
-          }, 200, function () {
-              $('#slider ul li:last-child').prependTo('#slider ul');
-              $('#slider ul').css('left', '');
-          });
-      };
-  
-      function moveRight() {
-          $('#slider ul').animate({
-              left: - slideWidth
-          }, 200, function () {
-              $('#slider ul li:first-child').appendTo('#slider ul');
-              $('#slider ul').css('left', '');
-          });
-      };
-  
-      $('a.control_prev').click(function () {
-          moveLeft();
-      });
-  
-      $('a.control_next').click(function () {
-          moveRight();
-      });
-  
-  });     
+});
+
+var slideShow = $("#slideshow") 
+
+
+  // Functional javascript for the slideshow div  
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000); 
+});
