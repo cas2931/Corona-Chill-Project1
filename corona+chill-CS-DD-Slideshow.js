1b7e8f39a442b60cd75ba85fe5c0998e2e8fac2f
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-26 21:27:21 -0400

    updated js file

--- corona+chill-CS-DD-Slideshow.js
+++ corona+chill-CS-DD-Slideshow.js
@@ -2,19 +2,21 @@ $(document).ready(function ($) {
 // Array of all movies in playlist
 var D_D_Movies = ['Outbreak','Contagion','28 Days Later','28 Weeks Later','12 Monkeys','I Am Legend',
                   'Rise of the Planet of the Apes','The Andromeda Strain','Cabin Fever','Flu','Deranged','Blindness'];  
+let D_D_Posters = []
+var slideShow = $("#CS-slideshow")
 
 // Array might require stringification to be used in a AJAX call, made variable just in case
 //var D_D_json = JSON.stringify(D_D_Movies); 
 
 //Loop to run through array
-for (i = 0; i < D_D_Movies.length; i++)
+for (i = 0; i < D_D_Movies.length; i++) {
 
 // Ajax call settings. Looks hinky compared to other api setups, but it was the way the maker set up the code snippet.
 // The method & url are the same, and the api key is already integrated in as a plus. 
 var AC_settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + D_D_Movies[0],
+	"url": "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + D_D_Movies[i],
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
@@ -23,22 +25,33 @@ var AC_settings = {
 }
 
 $.ajax(AC_settings).done(function (response) {
-  console.log(response);
-  
+  console.log(response);  
+  let {poster,title} = response
+  let img = $('<img>');
+  img.attr('src',poster); 
+  // slideShow.append(img)
+  D_D_Posters.push(poster)
 });
+} 
+function changeSlide(index) {
+if(index === D_D_Posters.length - 1){ 
+  return(D_D_Posters[0]); 
+  
 
-var slideShow = $("#slideshow") 
+} else return D_D_Posters[index + 1]
+}
 
 
   // Functional javascript for the slideshow div  
-$("#slideshow > div:gt(0)").hide();
-
+$("#CS-slideshow > div:gt(0)").hide();
+let posterIndex = 0
 setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  3000); 
+  let newPoster = changeSlide(posterIndex)
+    slideShow.fadeOut(500) 
+    setTimeout(function() {
+      slideShow.attr('src',newPoster)
+      .fadeIn(500)
+    },500) 
+   posterIndex ++
+},  6000); 
 });
